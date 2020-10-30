b7d4708a977f41110967401ed9d57ecaf2420e5e
samuelcheung924 <67924107+samuelcheung924@users.noreply.github.com>
2020-10-30 15:30:01 -0400

    lab 8 - Final

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,12 @@
 mydata = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+
+mykeys = []
+myvalues = []
+
 for k, v in mydata.items():
   print('key:', k, ', value:', v)
+  mykeys.append(k)
+  myvalues.append(v)
+
+print('All keys: ',mykeys)
+print('All values: ',myvalues)
